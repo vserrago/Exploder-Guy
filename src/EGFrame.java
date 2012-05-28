caae02d5c66f36023fc6858312ea5cf8f4752e19
Valentin <ogarres@gmail.com>
2012-05-28 17:40:57 -0400

    Centred window on spawn

--- src/EGFrame.java
+++ src/EGFrame.java
@@ -1,5 +1,7 @@
+import java.awt.Dimension;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
@@ -12,7 +14,14 @@ public class EGFrame extends JFrame
 	{
 		egpanel = new EGPanel();
 		setSize(EGPanel.WIDTH, EGPanel.HEIGHT);
+		
+		//Centre the window in the middle of the screen
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(screenSize.width/2 - EGPanel.WIDTH/2,
+				screenSize.height/2 - EGPanel.HEIGHT/2);
+		
 		add(egpanel);
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Exploder Guy");
 		setVisible(true);
